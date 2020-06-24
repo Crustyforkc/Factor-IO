bd32586e4e88d8ea77f389335e7f3830bf9fbbff
Chris.Ebling <mxebling@gmail.com>
2020-06-23 23:43:30 -0700

    Adjustments for Heroku

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
 	app.use(express.static('public'))
+	res.sendFile(path.join(__dirname + '/public/images/Background.png'));
 	res.sendFile(path.join(__dirname + '/public/game.html'));
 	
 });
