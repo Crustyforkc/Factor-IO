e01baa2ea97f7589add31fc7d89255a2b6969fba
Chris.Ebling <mxebling@gmail.com>
2020-06-30 17:00:06 -0700

    Attempt to remove sendfile

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
 	app.use(express.static('public'))
-	app.use(express.static('public/images'))
-	res.sendFile(path.join(__dirname + '/public/game.html'));
+	//app.use(express.static('public/images'))
+	//res.sendFile(path.join(__dirname + '/public/game.html'));
 	
 });
 
